13b61610a6123d81cb5e03aeaf4fd9d78d6f55fe
dongguncho <movegun23@gmail.com>
2023-03-15 22:14:46 +0900

    [Updated] 회원 라우트 수정

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -10,12 +10,9 @@ const userController = new UserController();
 router.get("/:userId", isAuthenticated(), userController.findUser);
 router.post("/login", userController.login);
 router.post("/", userController.addUser);
-<<<<<<< HEAD
 router.patch("/modify", isAuthenticated(), userController.modifyUser);
 router.delete("/:userId", isAuthenticated(), userController.removeUser);
-=======
 router.patch("/", isAuthenticated(), userController.modifyUser);
 router.delete("/", isAuthenticated(), userController.removeUser);
->>>>>>> 0ffe0514f7bfd9dc3664da37b244051f4981ede5
 
 export default router;
