4f0ad10662d431175653cacea93f8fb4b9ba8c4a
dongguncho <movegun23@gmail.com>
2023-04-26 19:59:39 +0900

    [Update] 쿠폰 라우트 추가

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,9 +2,11 @@ import express from "express";
 import userRouter from "./userRoute";
 import boardRouter from "./boardRoute";
 import boardCmtRouter from "./boardCmtRoute";
+import cuponRoute from "./cuponRoute";
 
 export default (app: express.Application) => {
   app.use("/api/auth", userRouter);
   app.use("/api/board", boardRouter);
   app.use("/api/boardCmt", boardCmtRouter);
+  app.use("/api/cupon", cuponRoute);
 };
