59e8678fc92c12824d6d6551e06c2e7ade061938
dongguncho <movegun23@gmail.com>
2023-05-21 21:38:33 +0900

    [Update] 쿠폰api 파라미터 변경

--- src/routes/cuponRoute.ts
+++ src/routes/cuponRoute.ts
@@ -8,6 +8,6 @@ const cuponController = new CuponController();
 router.get("/", isAuthenticated(), cuponController.findCupon);
 router.post("/", isAuthenticated(), cuponController.addCupon);
 router.patch("/", isAuthenticated(), cuponController.modifyCupon);
-router.delete("/", isAuthenticated(), cuponController.deleteCupon);
+router.delete("/:cuponId", isAuthenticated(), cuponController.deleteCupon);
 
 export default router;
